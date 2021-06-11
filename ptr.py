41a900cb8c1eb5088841370f674a219b809a9f4f
leafcool <leafcool@live.com>
2021-06-11 14:55:28 +0800

    leafcool
    重新修改后代码，更见简洁

--- ptr.py
+++ ptr.py
@@ -26,40 +26,6 @@
                   ┗┻┛  ┗┻┛ 
 """
 
-# import random
-#
-#
-# """建立一个随机数组用来验证代码"""
-# dict1={}  #使用字典K,V表示指针和其指向数值
-# n=random.randint(1,500) #假设数组变化个数
-# for i in range(n):
-#     dict1[i]=random.randint(0,10000) #每个键值对赋随机值
-#
-#
-# #计算移动一步后数组的K,V和b只计算下一步指针移动最大范围区间m
-# def function(a,b):
-#     arr=[]
-#     for i in range(a,b+1):
-#         arr.append(dict1[i]+i-b)
-#     return max(arr)  # return max positive shift
-#
-# #递归函数判断
-# def judgefunction(s1,s2):
-#     m=function(s1,s2)
-#     if m<1:
-#             print("can't go to the array[n-1]")
-#             return False
-#     else:
-#             s1=s2
-#             s2+=function(s1,s2)
-#             if s2>n-2:
-#                 print("successfully go to the end of array")
-#             else:
-#                 return judgefunction(s1,s2)
-#
-# s1=0
-# s2=dict1[0]
-# judgefunction(s1,s2)
 
 import random
 
@@ -69,6 +35,8 @@ n=random.randint(1,500) #设定数组一个随机长度
 for i in range(n):
     dict1[i]=random.randint(0,100) #每个键值对赋随机值
 
+# dict1={0:3,1:2,2:1,3:0,4:6}
+# n=len(dict1)
 
 """计算移动一步后计算下一步指针移动最大范围区间m"""
 def function(a,b):
@@ -81,22 +49,20 @@ def function(a,b):
 
 """递归判断函数"""
 def judgefunction(s1,s2):
-    m=function(s1,s2)
-    #m<1,无法正偏移量，说明此时结束
-    if m<1:
-        if s2==n-1:
-            print("successfully go to the end of array")
-        else:
-            print("can't go to the array[n-1]")
+    #判定s2指针能否覆盖到最后字典一位
+    if s2>n-2:
+        print("successfully go to the end of array")
     else:
-            s1=s2
-            s2+=function(s1,s2)
-            if s2>n-2:
-                print("successfully go to the end of array")
-            else:
+        m=function(s1,s2)
+        #m<1,无法正偏移量，说明此时结束
+        if m<1:
+            print("can't go to the array[n-1]")
+        # m>1,有正偏移量，继续调用函数本身
+        else:
+                s1=s2
+                s2+=function(s1,s2)
                 return judgefunction(s1,s2)
 
 s1=0
 s2=dict1[0]
 judgefunction(s1,s2)
-
